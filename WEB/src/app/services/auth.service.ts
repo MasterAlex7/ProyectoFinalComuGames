b94463d263f22bb1a14053d208bd91396aeb7585
Alex Ri <cabeacdc@gmail.com>
2022-12-10 20:38:39 -0600

    Creacion de Servicio AUTH

---
+++ WEB/src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { API, API_LOGIN, API_REGISTER } from './../config/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(strUsername: string, strPassword: string) {
+    const httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+    const params = JSON.stringify({strUsername: strUsername, strPassword: strPassword});
+    return this.http.post(API_LOGIN, params, {headers: httpHeaders});
+  }
+}
