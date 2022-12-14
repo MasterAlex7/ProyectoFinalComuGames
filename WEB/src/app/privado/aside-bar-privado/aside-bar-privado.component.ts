f71a9c49eb5490d2a3be286cdcaf29ae3ebda47c
Jose Rafael Gutierrez Padilla <rajopagu@gmail.com>
2022-12-13 21:00:45 -0600

    publicaciones 2

--- WEB/src/app/privado/aside-bar-privado/aside-bar-privado.component.ts
+++ WEB/src/app/privado/aside-bar-privado/aside-bar-privado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-aside-bar-privado',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsideBarPrivadoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  public Cambiar(){
+    this.router.navigate(['/crear-publicacion']);
   }
 
 }
--- WEB/src/app/privado/crear-publicacion/crear-publicacion.component.ts
+++ WEB/src/app/privado/crear-publicacion/crear-publicacion.component.ts
@@ -12,4 +12,8 @@ export class CrearPublicacionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public Enviar(){
+    
+  }
+
 }
